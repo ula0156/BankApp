b57fc10a3a4e34398f67edc7d220072eb7dd6ce9
Iuliia Aleksandrova <julie.aleksandrova@gmail.com>
2017-05-21 13:52:18 -0700

    Overloading and building a menu

--- BankApp/Account.cs
+++ BankApp/Account.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace bankApp
 {
+    public enum AccountTypes
+    {
+        Checking,
+        Savings,
+        CreditCard,
+        Loan
+    }
     class Account
     {
         private static int lastAccountNumber = 0;
@@ -15,7 +22,7 @@ namespace bankApp
         public decimal AccountNumber { get; private set; }
         public string EmailAddress { get; set; }
         public decimal Balance { get; private set; }
-        public string TypeOfAccount { get; set; }
+        public AccountTypes TypeOfAccount { get; set; }
         #endregion
 
         #region Constructor
@@ -24,6 +31,27 @@ namespace bankApp
             lastAccountNumber += 1;
             AccountNumber = lastAccountNumber;
         }
+
+        public Account(string accountName) : this()
+        {
+            AccountName = accountName;
+        }
+
+        public Account(string accountName, string emailAddress) : this(accountName)
+        {
+            EmailAddress = emailAddress;
+        }
+
+        public Account(string accountName, string emailAddress, AccountTypes typeOfAccount) : this(accountName, emailAddress)
+        {
+            TypeOfAccount = typeOfAccount;
+        }
+
+        public Account(string accountName, string emailAddress, AccountTypes typeOfAccount, decimal amount) : this(accountName, emailAddress, typeOfAccount)
+        {
+            Balance = amount;
+        }
+
 
         #endregion
 
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -6,15 +6,48 @@ namespace bankApp
     {
         static void Main(string[] args)
         {
-            // create a new instance of the account class 
-            // and set property for it;
-            var account = new Account();
-            account.AccountName = "My checking";
-            account.EmailAddress = "test@test.com";
-            account.TypeOfAccount = "Checking";
+            Console.WriteLine("*************Welcome to my Bank********");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("1. Create a new account");
+            Console.WriteLine("2: Deposit");
+            Console.WriteLine("3: Withdraw");
+            Console.WriteLine("4: Print all accounts");
 
-            account.Deposit(200);
-            Console.WriteLine($"AccountNumber : {account.AccountNumber}, Balance: {account.Balance}, EmailAddres: {account.EmailAddress}, TypeOfAccount: {account.TypeOfAccount}");
+            var choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "0":
+                    return;
+                case "1":
+                    Console.Write("Account name: ");
+                    var accountName = Console.ReadLine();
+                    Console.Write("Email Address: ");
+                    var emailAddress = Console.ReadLine();
+                    Console.WriteLine("Type of account: ");
+                    var accountTypes = Enum.GetNames(typeof(AccountTypes));
+                    for (int i = 0; i < accountTypes.Length; i++)
+                    {
+                        Console.WriteLine($"{i}. {accountTypes[i]}");
+                    }
+                    Console.Write("Pick an account type: ");
+                    var accountType = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Amount: ");
+                    var amount = Convert.ToDecimal(Console.ReadLine());
+                    var account = new Account
+                    {
+                        AccountName = accountName,
+                        EmailAddress = emailAddress,
+                        TypeOfAccount = (AccountTypes)accountType
+                    };
+                    account.Deposit(amount);
+                    Console.WriteLine($"AN: {account.AccountName}, Balance: {account.Balance}");
+                    break;
+                case "2":
+                case "3":
+                case "4":
+                default:
+                    break;
+            }
         }
     }
 }
