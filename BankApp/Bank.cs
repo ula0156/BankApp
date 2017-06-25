cb7d6585ae81a00cbdb87c5e24be3da71ea548e3
Iuliia Aleksandrova <julie.aleksandrova@gmail.com>
2017-06-25 10:38:58 -0700

    Creating static bank class

---
+++ BankApp/Bank.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankApp
+{
+    static class Bank
+    {
+        private static List<Account> accounts = new List<Account>();
+        public static string Name { get; set; }
+
+        // create a method for this class
+        public static Account CreateAccount(string accountName, string emailAddress, AccountTypes typeOfAccount, decimal amount)
+        {
+            var account = new Account
+            {
+                AccountName = accountName,
+                EmailAddress = emailAddress,
+                TypeOfAccount = typeOfAccount
+            };
+
+            if (amount > 0)
+            {
+                account.Deposit(amount);
+            }
+            accounts.Add(account);
+            return account;
+        }
+
+        public static List<Account> GetAllAccounts()
+        {
+            return accounts;
+        } 
+    }
+}
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -7,46 +7,45 @@ namespace bankApp
         static void Main(string[] args)
         {
             Console.WriteLine("*************Welcome to my Bank********");
-            Console.WriteLine("0. Exit");
-            Console.WriteLine("1. Create a new account");
-            Console.WriteLine("2: Deposit");
-            Console.WriteLine("3: Withdraw");
-            Console.WriteLine("4: Print all accounts");
-
-            var choice = Console.ReadLine();
-            switch (choice)
+            while (true)
             {
-                case "0":
-                    return;
-                case "1":
-                    Console.Write("Account name: ");
-                    var accountName = Console.ReadLine();
-                    Console.Write("Email Address: ");
-                    var emailAddress = Console.ReadLine();
-                    Console.WriteLine("Type of account: ");
-                    var accountTypes = Enum.GetNames(typeof(AccountTypes));
-                    for (int i = 0; i < accountTypes.Length; i++)
-                    {
-                        Console.WriteLine($"{i}. {accountTypes[i]}");
-                    }
-                    Console.Write("Pick an account type: ");
-                    var accountType = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Amount: ");
-                    var amount = Convert.ToDecimal(Console.ReadLine());
-                    var account = new Account
-                    {
-                        AccountName = accountName,
-                        EmailAddress = emailAddress,
-                        TypeOfAccount = (AccountTypes)accountType
-                    };
-                    account.Deposit(amount);
-                    Console.WriteLine($"AN: {account.AccountName}, Balance: {account.Balance}");
-                    break;
-                case "2":
-                case "3":
-                case "4":
-                default:
-                    break;
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Create a new account");
+                Console.WriteLine("2: Deposit");
+                Console.WriteLine("3: Withdraw");
+                Console.WriteLine("4: Print all accounts");
+
+                var choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "0":
+                        return;
+                    case "1":
+                        Console.Write("Account name: ");
+                        var accountName = Console.ReadLine();
+                        Console.Write("Email Address: ");
+                        var emailAddress = Console.ReadLine();
+                        Console.WriteLine("Type of account: ");
+                        var accountTypes = Enum.GetNames(typeof(AccountTypes));
+                        for (int i = 0; i < accountTypes.Length; i++)
+                        {
+                            Console.WriteLine($"{i}. {accountTypes[i]}");
+                        }
+                        Console.Write("Pick an account type: ");
+                        var accountType = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Amount: ");
+                        var amount = Convert.ToDecimal(Console.ReadLine());
+
+                        // Because bank class is the static class, we don't use word new for constructor function
+                        var account = Bank.CreateAccount(accountName, emailAddress, (AccountTypes)accountType, amount);
+                        Console.WriteLine($"AN: {account.AccountName}, Balance: {account.Balance: C}");
+                        break;
+                    case "2":
+                    case "3":
+                    case "4":
+                    default:
+                        break;
+                }
             }
         }
     }
