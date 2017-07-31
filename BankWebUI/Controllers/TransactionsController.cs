ecb1f0715daed16877083f70727c6b94d498605f
Iuliia Aleksandrova <iuliia.aleksandrova@yahoo.com>
2017-07-31 11:44:56 -0700

    Removing database

---
+++ BankWebUI/Controllers/TransactionsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using bankApp;
+
+namespace BankWebUI.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        // GET: Transactions
+        public ActionResult Index(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var transactions = Bank.GetAllTransacationForAccount(id.Value);
+            return View(transactions);
+        }
+    }
+}
